b0f40e6a538fa505c41e033725ab72c4486a32eb
shuaibfarouk <35780828+shuaibfarouk@users.noreply.github.com>
2024-07-21 11:11:42 +0300

    map.js

--- map.js
+++ map.js
@@ -1,41 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Google Maps with Data-Driven Styling</title>
-    <!-- Google Tag Manager -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMVLNX9NYH"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-NMVLNX9NYH');
-    </script>
-    <!-- Load the Maps JavaScript API with v=weekly -->
-    <script>
-        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-            key: "AIzaSyCobCO1jQNA8cIv-IwQU9uzR9qoQaqvFiY",
-            v: "weekly",
+let map;
+
+async function initMap() {
+    await google.maps.importLibrary("maps");
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 29.478324, lng: 47.607149},
+        zoom: 13,
+        mapId: '7241fd4fdf5c0624'
+    });
+
+    // Load GeoJSON file
+    map.data.loadGeoJson('https://shuaibfarouk.github.io/AlMutlaa/entities.geojson', null, () => {
+        // Style the dataset
+        map.data.setStyle(feature => {
+            return {
+                fillColor: 'green',
+                fillOpacity: 1,
+                strokeColor: 'black',
+                strokeWeight: 1
+            };
         });
-    </script>
-    <style>
-        /* Make the map full screen */
-        #map {
-            height: 100%;
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-        html, body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-    </style>
-</head>
-<body>
-    <div id="map"></div>
-    <!-- Load your custom JavaScript file -->
-    <script src="map.js"></script>
-</body>
-</html>
+
+        // Add click event to show info window
+        const infoWindow = new google.maps.InfoWindow();
+
+        map.data.addListener('click', event => {
+            const feature = event.feature;
+            const content = `<div><strong>Building ID:</strong> ${feature.getProperty('building_id')}</div>`;
+            infoWindow.setContent(content);
+            infoWindow.setPosition(event.latLng);
+            infoWindow.open(map);
+        });
+    });
+}
+
+window.initMap = initMap;
