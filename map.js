3638c52d4b80b243de41fac4b7c8caf43b625c58
shuaibfarouk <35780828+shuaibfarouk@users.noreply.github.com>
2024-07-22 08:35:20 +0300

    map.js

--- map.js
+++ map.js
@@ -4,13 +4,15 @@ async function initMap() {
     await google.maps.importLibrary("maps");
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 29.478324, lng: 47.607149 },
+        center: {lat: 29.478324, lng: 47.607149},
         zoom: 13,
         mapId: '7241fd4fdf5c0624'
     });
 
+    // Load the GeoJSON file from GitHub
     map.data.loadGeoJson('https://shuaibfarouk.github.io/AlMutlaa/entities.geojson');
 
+    // Style the GeoJSON data
     map.data.setStyle({
         fillColor: 'green',
         fillOpacity: 1,
@@ -18,6 +20,7 @@ async function initMap() {
         strokeWeight: 1
     });
 
+    // Add click event to show info window
     const infoWindow = new google.maps.InfoWindow();
 
     map.data.addListener('click', event => {
@@ -29,4 +32,4 @@ async function initMap() {
     });
 }
 
-window.initMap = initMap;
+initMap();
